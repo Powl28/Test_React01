787ffc36d68f349cec88b76899e7d7178f7bdf0a
Surakit <surakit28_onenonly@hotmail.com>
2024-07-06 15:13:04 +0700

    app01-props from parent to child

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from './components/Header'
 import StudentList from './components/StudentList'
 
 function App() {
+  let students = [];
+
+  for (let index = 0; index < 5; index++) {
+    let student = {
+      id: index,
+      name: "STUDENT_" + index,
+    };
+
+    students.push(student);
+  }
+
+  const [data, setData] = useState(students);
+  const [show, setShow] = useState(true);
+
+  const deleteData = (id) => {
+    setData(data.filter((item) => item.id != id));
+  };
+
+  
   return (
     <div>
 
       <Header title="Student List"  major="major"/>
-      <StudentList/>
+      <StudentList data={data} deleteData={deleteData} />
 
     </div>
   )
--- app01-state/src/components/StudentList.jsx
+++ app01-state/src/components/StudentList.jsx
@@ -1,24 +1,11 @@
 import React, { useState } from "react";
 import "./StudentList.css";
 
-function StudentList() {
-  let students = [];
-
-  for (let index = 0; index < 5; index++) {
-    let student = {
-      id: index,
-      name: "STUDENT_" + index,
-    };
-
-    students.push(student);
-  }
-
-  const [data, setData] = useState(students);
+function StudentList({data,deleteData}) {
+    
   const [show, setShow] = useState(true);
 
-  const deleteData = (id) => {
-    setData(data.filter((item) => item.id != id));
-  };
+
 
   const table = () => (
     <table class="table  table-striped">
@@ -51,17 +38,19 @@ function StudentList() {
 
   return (
     <main>
-        <div className="summary">
-        <p style={{backgroundColor:'cyan',fontStyle:'italic'}}>AllStuden : {data.length}</p>
-          <button
-            onClick={() => setShow(!show)}
-            type="button"
-            className="btn btn=success"
-          >
-            {show ? "HIDDEN" : "SHOW"}
-          </button>
-        </div>
-        {show && table()}
+      <div className="summary">
+        <p style={{ backgroundColor: "cyan", fontStyle: "italic" }}>
+          AllStuden : {data.length}
+        </p>
+        <button
+          onClick={() => setShow(!show)}
+          type="button"
+          className="btn btn=success"
+        >
+          {show ? "HIDDEN" : "SHOW"}
+        </button>
+      </div>
+      {show && table()}
     </main>
   );
 }
