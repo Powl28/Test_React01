29f6c52904d1611a82605cdba3a58af8b90c27fa
Surakit <surakit28_onenonly@hotmail.com>
2024-07-06 08:37:47 +0700

    Show_And_Hidden

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
     let student = {
       id : index,
-      name : "student" + index,
+      name : "STUDENT" + index,
     };
     
     students.push(student);
@@ -15,32 +15,43 @@ function App() {
 
   const [data, setData] = useState(students);
 
+  const [show, setShow] = useState(true)
+
   const deleteData = (id)=>{
     setData(data.filter((item)=>item.id != id))
   } 
 
+  const table = ()=>(     
+  <table class="table  table-striped">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">ACTION</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((item)=>
+        (
+           <tr key={item.id} >
+        <th scope="row">{item.id}</th>
+        <td>{item.name}</td>
+        <td><button onClick={()=>deleteData(item.id)} className="btn btn=danger">Delete</button></td>
+      </tr>
+        ))}
+    </tbody>
+  </table>)
+
+
   return (
     <div>
-       <table class="table  table-striped">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">ACTION</th>
+   
+    {show && table()}
 
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item)=>
-          (
-             <tr key={item.id} >
-          <th scope="row">{item.id}</th>
-          <td>{item.name}</td>
-          <td><button onClick={()=>deleteData(item.id)} className="btn btn=danger">Delete</button></td>
-        </tr>
-          ))}
-      </tbody>
-    </table>
+
+    <td><button onClick={()=>setShow(!show)} className="btn btn=danger">{show ?"HIDDEN":"SHOW"}</button></td>
+
     </div>
 
    
